9a15618a0c3efff7dd980273004df5bd099310ee
Ahmed Hatem <ah939309@gmail.com>
2024-12-18 21:16:23 +0200

    Fix Home Bug

--- lib/models/providers_model.dart
+++ lib/models/providers_model.dart
@@ -15,7 +15,7 @@ class ProvidersModel {
       });
     }
   }
-
+// From ابو  شيماء لابو عبير
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['message'] = this.message;
